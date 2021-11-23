ae3aefd42e167e7d5f8f13c5cd68316c534aaff1
Michael Jenkins <jenkinsmichpa@gmail.com>
2021-11-23 04:09:24 -0500

    Create bastet.sh

---
+++ bastet.sh
@@ -0,0 +1,35 @@
+# Bastet, a netcat activity detector for CNIT 470
+# by jenkinsmichpa inspired by theif-adj / aeris salus / cardea scripts
+
+discbinBastet='/home/cnit470g45/discbin/discbin <DISCORD WEBHOOK HERE>'
+packetSource='/captures/captureFile*'
+localNetwork='10.51.<GROUP NUMBER HERE>.0/24'
+netcatProtocol='tcp'
+netcatPort='9999'
+
+touch -a bastet.log
+touch -a bastet.err
+touch -a bastet.res
+
+for file in $packetSource; do
+
+  previousCompleteResults=$(grep "${file} complete" bastet.res)
+  if [ -n "$previousCompleteResults" ]; then
+    continue
+  fi
+
+  badKitties=$(tshark -r "$file" -Y "ip.dst==${localNetwork} && ${netcatProtocol}.dstport == ${netcatPort}" 2> bastet.err | awk '//') # me and subshells have trust issues
+
+  if [[ $badKitties =~ $netcatPort ]]; then
+    echo pawsitive "$file" complete >> bastet.res
+    echo "$badKitties" >> bastet.log
+    echo "Mwow? Netcat reverse shell activity detected in ${file}! ฅ(=ዎܫዎ=)∫" | $discbinBastet
+    echo "$badKitties" | $discbinBastet
+  else
+    echo nyagative "$file" complete >> bastet.res
+  fi
+
+done
+
+sed -i '/incomplete/d' bastet.res
+sed -i 's/\(.*\)complete/\1incomplete/' bastet.res
